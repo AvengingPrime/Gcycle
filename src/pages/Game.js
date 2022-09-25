8a7b6d004c28df04d962eb7cc5e9f1e3961fdf31
andrew <andrewsenth@gmail.com>
2022-09-25 04:00:11 -0500

    embedded unity app

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 const Game = () => {
-    return (
-        <>
-            
-        </>
-    );
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "UnityApp/Build/build.loader.js",
+        dataUrl: "UnityApp/Build/build.data",
+        frameworkUrl: "UnityApp/Build/build.framework.js",
+        codeUrl: "UnityApp/Build/build.wasm",
+    });
+
+    return <Unity unityProvider={unityProvider} />;
 }
 
 export default Game;
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Results = (props) => {
     const [photoTaken, setPhotoTaken] = useState(false);
 
